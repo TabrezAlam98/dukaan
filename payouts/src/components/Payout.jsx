f9a0b70ea8ae7e8df5464223338f71e83dac2cf4
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2023-12-23 09:51:42 +0530

    added table

--- payouts/src/components/Payout.jsx
+++ payouts/src/components/Payout.jsx
@@ -1,100 +1,396 @@
 import React from "react";
-import { Box, Flex, Image, Text,Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Spacer,
+  Input,
+  Select,
+  Button,
+} from "@chakra-ui/react";
+
+import Tab from "./Tab";
 
 const Payout = () => {
   return (
     <Flex w="100%" border="1px" borderColor="green">
-
       {/* ............... Nav Box........... */}
-      <Box w="15%" height="100%" pb="15px" border="1px" borderColor="gray.200" bg="#1E2640" color="#FFFFFF">
-        <Flex w="90%" h="35px" m="auto" mt="10px" border="1px" borderColor="yellow">
-          <Box>
-            <Image
-              w="35px"
-              h="100%"
-              src="https://bit.ly/dan-abramov"
-              alt="Dan Abramov"
-            />
+      <Box
+        w="15%"
+        height="100%"
+        pb="15px"
+        position="fixed"
+        bg="#1E2640"
+        color="#FFFFFF"
+      >
+        <Flex w="90%" h="35px" m="auto" mt="10px">
+          <Box w="35px" bg="white" borderRadius="5px">
+            <Text
+              fontSize="8px"
+              color="red"
+              textAlign="center"
+              mt="10px"
+              fontWeight="700"
+            >
+              {" "}
+              Nishyan
+            </Text>
           </Box>
-          <Spacer/>
+          <Spacer />
           <Box ml="-50px">
-            <Text fontSize="14px" lineHeight='20px' fontWeight="500" ml='-2px'>Nishyan</Text>
-            <Text fontSize="11px"><a href=""><u>Visit Store</u></a></Text>
+            <Text fontSize="14px" lineHeight="20px" fontWeight="500" ml="-2px">
+              Nishyan
+            </Text>
+            <Text fontSize="11px">
+              <a href="">
+                <u>Visit Store</u>
+              </a>
+            </Text>
           </Box>
-          <Spacer/>
+          <Spacer />
           <Box mt="3px">
-            <i  class="fa-solid fa-angle-down"></i>
+            <i class="fa-solid fa-angle-down"></i>
           </Box>
         </Flex>
-        <Box w="100%" h="100%" m="auto" mt="10px" border="1px" borderColor="red.500">
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i  class="fa-solid fa-house" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Home</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-suitcase-rolling" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Orders</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-bars" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Products</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-truck" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Delivery</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-suitcase" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Marketing</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-chart-simple" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Analytics</Text>
-           </Flex>
-           <Flex w="90%" m="auto" borderRadius="5px"  p="8px 16px 8px 16px" bg="gray.600"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-credit-card" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Payouts</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-gear" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Discount</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-regular fa-user" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Audience</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-palette" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Apearance</Text>
-           </Flex>
-           <Flex  p="8px 16px 8px 16px"  fontSize="14px" lineHeight='20px' fontWeight="500">
-           <i class="fa-solid fa-bolt" w="20px" h="20px"></i>
-            <Text ml="10px" mt="-5px">Plugins</Text>
-           </Flex>
-           <Flex bg="gray.600"  w="90%"  m="auto" mt="90px" p="8px" border="1px" borderRadius="5px">
-          <Box>
-          <i class="fa-solid fa-wallet" w="23px" h="20px"></i>
-          </Box>
-          {/* <Spacer /> */}
-          <Box>
-            <Text  fontSize="12px"  ml="15px">Available credits</Text>
-            <Text  fontSize="14px"  mr="30px">222.10</Text>
-          </Box>
-        
-        </Flex>
+        <Box w="100%" h="100%" m="auto" mt="10px">
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-house" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Home
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-suitcase-rolling" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Orders
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-bars" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Products
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-truck" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Delivery
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-suitcase" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Marketing
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-chart-simple" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Analytics
+            </Text>
+          </Flex>
+          <Flex
+            w="90%"
+            m="auto"
+            borderRadius="5px"
+            p="8px 16px 8px 16px"
+            bg="gray.600"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-credit-card" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Payouts
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-gear" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Discount
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-regular fa-user" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Audience
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-palette" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Apearance
+            </Text>
+          </Flex>
+          <Flex
+            p="8px 16px 8px 16px"
+            fontSize="14px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            <i class="fa-solid fa-bolt" w="20px" h="20px"></i>
+            <Text ml="10px" mt="-5px">
+              Plugins
+            </Text>
+          </Flex>
+          <Flex
+            bg="gray.600"
+            w="90%"
+            m="auto"
+            mt="80px"
+            p="8px"
+            border="1px"
+            borderRadius="5px"
+          >
+            <Box>
+              <i class="fa-solid fa-wallet" w="23px" h="20px"></i>
+            </Box>
+
+            <Box>
+              <Text fontSize="12px" ml="15px">
+                Available credits
+              </Text>
+              <Text fontSize="14px" mr="30px">
+                222.10
+              </Text>
+            </Box>
+          </Flex>
         </Box>
-        
       </Box>
-     
-     {/* ..........Main Box.......... */}
-      <Box w="85%" border="1px" borderColor="red.200">
-      <Flex border="1px" borderColor="red.400">
-        <Box w="30%">
-          <Text>How it works</Text>
+
+      {/* ..........Main Box.......... */}
+      <Box w="85%" position="absolute" right="0">
+        <Flex p="5px">
+          <Box w="30%">
+            <Text fontSize="12px" mt="4px">
+              How it works
+            </Text>
+          </Box>
+
+          <Box w="30%">
+            <Flex h="25px" bg="#F2F2F2" borderRadius="5px">
+              <Box ml="5px" fontSize="12px" mt="3px">
+                <i class="fas fa-magnifying-glass"></i>
+              </Box>
+
+              <Input
+                h="100%"
+                fontSize="12px"
+                placeholder="Search features, tutorials etc."
+              />
+            </Flex>
+          </Box>
+          <Spacer />
+          <Box w="10%">
+            <Flex>
+              <Box w="25px" h="100%" bg="#F2F2F2" borderRadius="10px">
+                <i class="fa-solid fa-bullhorn"></i>
+              </Box>
+              <Box ml="10px" w="25px" h="100%" bg="#F2F2F2" borderRadius="50%">
+                <i class="fa-solid fa-caret-down"></i>
+              </Box>
+            </Flex>
+          </Box>
+        </Flex>
+
+        {/* .......overView Box......... */}
+        <Box h="100%" bg="#F2F2F2">
+          <Flex w="90%" m="auto" mt="5px">
+            <Text fontSize="16px" lineHeight="20px" fontWeight="500">
+              Overview
+            </Text>
+            <Spacer />
+            <Select
+              w="13%"
+              bg="white"
+              mt="5px"
+              fontSize="14px"
+              lineHeight="20px"
+              fontWeight="500"
+              placeholder="This month"
+            >
+              <option value="option1">Option 1</option>
+            </Select>
+          </Flex>
+
+          <Flex w="90%" m="auto" mt="15px">
+            <Box color="white" w="30%" borderRadius="5px" bg="#146EB4">
+              <Text fontSize="10px" mt="12px" ml="7px" textAlign="left">
+                Next Payout
+              </Text>
+              <Flex p="6px">
+                <Text fontSize="18px">₹2,321.23</Text>
+                <Spacer />
+                <Text fontSize="12px">
+                  <u>23 orders</u>
+                </Text>
+              </Flex>
+              <Flex
+                fontSize="10px"
+                p="7px"
+                mt="10px"
+                bg="#0E4F82"
+                borderBottomLeftRadius="5px"
+                borderBottomRightRadius="5px"
+              >
+                <Text>Next payout date:</Text>
+                <Spacer />
+                <Text>Today,04:00PM</Text>
+              </Flex>
+            </Box>
+            <Spacer />
+            <Box
+              color="black"
+              w="30%"
+              h="80%"
+              pb="10px"
+              borderRadius="5px"
+              bg="white"
+            >
+              <Text fontSize="10px" mt="12px" ml="7px" textAlign="left">
+                Amount Pending
+              </Text>
+              <Flex p="6px">
+                <Text fontSize="18px">₹92,312.30</Text>
+                <Spacer />
+                <Text fontSize="12px" color="#146EB4">
+                  <u>13 orders</u>
+                </Text>
+              </Flex>
+            </Box>
+            <Spacer />
+            <Box
+              color="black"
+              w="30%"
+              h="80%"
+              pb="10px"
+              borderRadius="5px"
+              bg="white"
+            >
+              <Text fontSize="10px" mt="12px" ml="7px" textAlign="left">
+                Amount Processes
+              </Text>
+
+              <Text fontSize="18px" textAlign="left" ml="7px" mt="5px">
+                ₹23,92,312.19
+              </Text>
+            </Box>
+          </Flex>
+
+          <Text
+            textAlign="left"
+            m="20px 0 0 50px"
+            fontSize="16px"
+            lineHeight="20px"
+            fontWeight="500"
+          >
+            Transactions | This Month
+          </Text>
+          <Button
+            ml="-70%"
+            mt="10px"
+            h="25px"
+            fontSize="12px"
+            bg="#146EB4"
+            color="white"
+            borderRadius="20px"
+          >
+            Payouts (22)
+          </Button>
+          <Button
+            ml="15px"
+            mt="10px"
+            h="25px"
+            fontSize="12px"
+            borderRadius="20px"
+            bg="#808080"
+            color="#F2F2F2"
+          >
+            Refunds (2)
+          </Button>
+          <Box p="5px" w="90%" m="auto" mt="10px" bg="white">
+            <Flex w="100%">
+              <Box w="15%" border="1px" borderColor="gray.400">
+                <Flex h="25px">
+                  <Box ml="5px" fontSize="12px" mt="3px">
+                    <i class="fas fa-magnifying-glass"></i>
+                  </Box>
+
+                  <Input h="100%" fontSize="12px" />
+                </Flex>
+              </Box>
+              <Spacer />
+              <Box w="20%" mr="-108px">
+                <Flex>
+                  <Box h="100%" border="1px" borderColor="gray.400">
+                    <Flex p="2px 5px 2px 5px">
+                      <Text fontSize="12px">Sort</Text>
+                      <Box ml="5px" fontSize="12px">
+                        <i class="fa-solid fa-arrow-up-wide-short"></i>
+                      </Box>
+                    </Flex>
+                  </Box>
+                  <Box
+                    ml="10px"
+                    fontSize="12px"
+                    p="2px"
+                    h="100%"
+                    border="1px"
+                    borderColor="gray.400"
+                  >
+                    <i class="fa-solid fa-download"></i>
+                  </Box>
+                </Flex>
+              </Box>
+            </Flex>
+            <Tab />
+          </Box>
         </Box>
-        <Box></Box>
-        <Box></Box>
-      </Flex>
       </Box>
     </Flex>
   );
---
+++ payouts/src/components/Tab.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import {
+    Box,
+    Flex,
+    Tr,
+    Th,
+    Td,
+    Thead,
+    Table,
+    Tbody,
+    TableContainer,
+  } from "@chakra-ui/react";
+
+const Tab = () => {
+  return (
+    <TableContainer mt="10px">
+    <Table variant="simple">
+      <Thead p="5px" fontSize="12px">
+        <Tr bg="#F2F2F2">
+          <Th>
+            <Box ml="10px" w="25px" h="100%">
+              <i class="fa-solid fa-caret-down"></i>
+            </Box>
+          </Th>
+          <Th></Th>
+          <Th>Transaction ID</Th>
+          <Th>Order amount</Th>
+          <Th isNumeric>Transaction fees</Th>
+          <Th isNumeric>Total</Th>
+        </Tr>
+      </Thead>
+      <Tbody fontSize="12px" bg="white">
+        <Tr>
+          <Td></Td>
+          <Flex>
+            <Box
+              mt="22px"
+              mr="-20px"
+              w="8px"
+              h="8px"
+              borderRadius="50%"
+              bg="green"
+            >
+           
+            </Box>
+            <Td>Processing</Td>
+          </Flex>
+
+          <Td isNumeric>131634495747</Td>
+          <Td isNumeric>₹10,125.00</Td>
+          <Td isNumeric>₹1,125.00</Td>
+          <Td isNumeric>₹9,12.00</Td>
+        </Tr>
+        <Tr>
+          <Td></Td>
+          <Flex>
+            <Box
+              mt="22px"
+              mr="-20px"
+              w="8px"
+              h="8px"
+              borderRadius="50%"
+              bg="green"
+            >
+           
+            </Box>
+            <Td>Processing</Td>
+          </Flex>
+
+          <Td isNumeric>131634495747</Td>
+          <Td isNumeric>₹10,125.00</Td>
+          <Td isNumeric>₹1,125.00</Td>
+          <Td isNumeric>₹9,12.00</Td>
+        </Tr>
+        <Tr>
+          <Td></Td>
+          <Flex>
+            <Box
+              mt="22px"
+              mr="-20px"
+              w="8px"
+              h="8px"
+              borderRadius="50%"
+              bg="green"
+            >
+           
+            </Box>
+            <Td>Processing</Td>
+          </Flex>
+
+          <Td isNumeric>131634495747</Td>
+          <Td isNumeric>₹10,125.00</Td>
+          <Td isNumeric>₹1,125.00</Td>
+          <Td isNumeric>₹9,12.00</Td>
+        </Tr>
+        <Tr>
+          <Td></Td>
+          <Flex>
+            <Box
+              mt="22px"
+              mr="-20px"
+              w="8px"
+              h="8px"
+              borderRadius="50%"
+              bg="green"
+            >
+           
+            </Box>
+            <Td>Processing</Td>
+          </Flex>
+
+          <Td isNumeric>131634495747</Td>
+          <Td isNumeric>₹10,125.00</Td>
+          <Td isNumeric>₹1,125.00</Td>
+          <Td isNumeric>₹9,12.00</Td>
+        </Tr>
+        <Tr>
+          <Td></Td>
+          <Flex>
+            <Box
+              mt="22px"
+              mr="-20px"
+              w="8px"
+              h="8px"
+              borderRadius="50%"
+              bg="green"
+            >
+           
+            </Box>
+            <Td>Processing</Td>
+          </Flex>
+
+          <Td isNumeric>131634495747</Td>
+          <Td isNumeric>₹10,125.00</Td>
+          <Td isNumeric>₹1,125.00</Td>
+          <Td isNumeric>₹9,12.00</Td>
+        </Tr>
+      </Tbody>
+    </Table>
+  </TableContainer>
+  )
+}
+
+export default Tab
